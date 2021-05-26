d8d8c199f5827e45a42ed9869c2d84fec810ff86
RazorSnow <changjiangmingxj@163.com>
2021-05-26 16:42:43 +0800

    commit

---
+++ src/SimplePublisher.java
@@ -0,0 +1,38 @@
+import java.util.Iterator;
+import java.util.concurrent.Flow;
+import java.util.stream.IntStream;
+
+public class SimplePublisher implements Flow.Publisher<Integer> {
+
+    private final Iterator<Integer> iterator;
+
+    SimplePublisher(int count) {
+        this.iterator = IntStream.rangeClosed(1, count).iterator();
+    }
+
+    @Override
+    public void subscribe(Flow.Subscriber<? super Integer> subscriber) {
+        iterator.forEachRemaining(subscriber::onNext);
+        subscriber.onComplete();
+    }
+
+    public static void main(String[] args) {
+        new SimplePublisher(10).subscribe(new Flow.Subscriber<>() {
+            @Override
+            public void onSubscribe(Flow.Subscription subscription) {}
+
+            @Override
+            public void onNext(Integer item) {
+                System.out.println("item = [" + item + "]");
+            }
+
+            @Override
+            public void onError(Throwable throwable) {}
+
+            @Override
+            public void onComplete() {
+                System.out.println("complete");
+            }
+        });
+    }
+}
