954fea88ad7754a2dd2e822afb8ebabbb1d0a5b0
RazorSnow <changjiangmingxj@163.com>
2021-05-26 22:31:47 +0800

    commit

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,11 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println(test((a, b) -> {
-            return a + b ;
+        System.out.println(execute((a,b) -> {
+            return 60 ;
         }));
     }
 
-    public static int test(FunctionalInterface functionalInterface) {
+    public static int execute(FunctionalInterface functionalInterface) {
         return functionalInterface.test(5, 6);
     }
 }
