f3fd04ff188775d5b5fef55d2a23ced35adfa0d2
RazorSnow <changjiangmingxj@163.com>
2021-05-27 11:41:41 +0800

    commit

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,20 @@
+import java.io.*;
+
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int result = execute((a, b, c) -> 60);
         System.out.println(result);
+        String txt = processFile((br) ->
+                br.readLine() + " " + br.readLine()
+        );
+        System.out.println(txt);
+    }
+
+    public static String processFile(BufferedReaderProcessor p) throws IOException {
+        try (BufferedReader br =
+                     new BufferedReader(new FileReader("src/data.txt"))) {
+            return p.process(br);
+        }
     }
 
     public static int execute(FunctionalInterface functionalInterface) {
