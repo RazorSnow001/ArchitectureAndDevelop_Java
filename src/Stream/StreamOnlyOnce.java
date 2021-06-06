36e5569d55099d651eec2c424d4af5035b32b762
RazorSnow <changjiangmingxj@163.com>
2021-06-06 21:46:49 +0800

    commit

--- src/Stream/StreamOnlyOnce.java
+++ src/Stream/StreamOnlyOnce.java
@@ -11,7 +11,7 @@ public class StreamOnlyOnce {
         List<String> title = Arrays.asList("Modern", "Java", "In", "Action");
         Stream<String> s = title.stream();
         List<String> result = s.filter((d) -> d.length() > 2).collect(toList());
-        System.out.println(result.toArray());
+        System.out.println(result);
         /* s.forEach(System.out::println);*/
         /*s.forEach(System.out::println);*/
     }
