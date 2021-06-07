f5b73f0875f2d32e51693012d7c9b84960c7fc51
RazorSnow <changjiangmingxj@163.com>
2021-06-07 19:02:09 +0800

    commit

---
+++ src/Stream/FindFirst.java
@@ -0,0 +1,14 @@
+package Stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class FindFirst {
+    public static void main(String[] args) {
+        List<Integer> someNumbers = Arrays.asList(1,2,3,4,5,6);
+        someNumbers.stream().map(n->n*n)
+                .filter(n->n%3==0)
+                .findFirst().ifPresent(System.out::println);
+    }
+}
