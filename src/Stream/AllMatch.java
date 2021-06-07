092467fc361f2d155fe03407a6eb047150d7af96
RazorSnow <changjiangmingxj@163.com>
2021-06-07 18:46:02 +0800

    commit

---
+++ src/Stream/AllMatch.java
@@ -0,0 +1,17 @@
+package Stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AllMatch {
+    public static void main(String[] args) {
+        List<Dish> specialMenu = Arrays.asList(
+                new Dish("seasonal fruit", true, 120),
+                new Dish("prawns", false, 300),
+                new Dish("rice", true, 350),
+                new Dish("chicken", false, 400),
+                new Dish("french fries", true, 530));
+        boolean result = specialMenu.stream().allMatch(d -> d.getCalories() > 100);
+        System.out.println(result);
+    }
+}
