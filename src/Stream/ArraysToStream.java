8e14b10fef53f7e0fa95a9caf3490e79dda437a5
RazorSnow <changjiangmingxj@163.com>
2021-06-07 18:08:10 +0800

    commit

--- src/Stream/ArraysToStream.java
+++ src/Stream/ArraysToStream.java
@@ -1,6 +1,7 @@
 package Stream;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class ArraysToStream {
@@ -9,5 +10,8 @@ public class ArraysToStream {
         String[] testArray = test.split("");
         Stream<String> testStream = Arrays.stream(testArray);
         testStream.forEach(System.out::printf);
+        List<String> words = Arrays.asList("We", "like", "basketball", "football", "baseball", "and", "so", "on");
+        System.out.println();
+        words.stream().map(word -> word.split("")).flatMap(Arrays::stream).distinct().forEach(System.out::printf);
     }
 }
