fd1811e87fe2b0a0769ab36cdac776db5dbe65f0
RazorSnow <changjiangmingxj@163.com>
2021-06-07 19:54:05 +0800

    commit

---
+++ src/Stream/Reduction.java
@@ -0,0 +1,15 @@
+package Stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class Reduction {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1,3,9,10,55,2);
+        int result = numbers.stream().reduce(0,Integer::sum);
+        System.out.println(result);
+        Optional<Integer> max = numbers.stream().reduce(Integer::max);
+        System.out.println(max);
+    }
+}
