bef0e4cb717d052f2d1f621b7f11321af6f1fc7c
RazorSnow <changjiangmingxj@163.com>
2021-08-02 21:07:13 +0800

    priority queue

---
+++ src/main/java/QUEUE/PriorityQueue.java
@@ -0,0 +1,11 @@
+package QUEUE;
+
+public class PriorityQueue {
+    public static void main(String[] args) {
+        java.util.PriorityQueue<Integer> queue = new java.util.PriorityQueue(10, (o1, o2) -> (Integer)o2-(Integer)o1);
+        queue.add(4);
+        queue.add(3);
+        queue.add(12);
+        System.out.println(queue.poll());
+    }
+}
