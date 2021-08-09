768d61a83abbe3e9163b200ab22be929fa150c17
RazorSnow <changjiangmingxj@163.com>
2021-08-09 23:48:13 +0800

    asyn and multiple threads programming ! Rxjava

---
+++ src/main/java/AsynchronousFramework/RxJava/TestObservable.java
@@ -0,0 +1,25 @@
+package AsynchronousFramework.RxJava;
+
+import io.reactivex.rxjava3.core.Observable;
+
+public class TestObservable {
+    public static void main(String[] args) {
+        Observable<String> source = Observable.create(emitter -> {
+            emitter.onNext("Alpha");
+            emitter.onNext("Beta");
+            emitter.onNext("Gamma");
+            emitter.onNext("Delta");
+            emitter.onNext("Epsilon");
+            emitter.onComplete();
+        });
+        source.subscribe(s -> System.out.println("RECEIVED: " + s));
+/*Note that the Observable contract dictates that
+emissions must be passed sequentially and one at a time. Emissions cannot be passed by
+an Observable concurrently or in parallel. This may seem like a limitation, but it does in
+fact simplify programs and make Rx easier to reason with. We will learn some powerful
+tricks to effectively leverage concurrency and parallelization in Chapter 6, Concurrency
+and Parallelization , without breaking the Observable contract.
+*/
+
+    }
+}
