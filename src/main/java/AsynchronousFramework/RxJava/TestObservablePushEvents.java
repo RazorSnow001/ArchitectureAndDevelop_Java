a08814560ab4bb2e781bb5c2e28213a12ba6c32a
RazorSnow <changjiangmingxj@163.com>
2021-08-09 23:22:19 +0800

    asyn and multiple threads programming ! Rxjava

---
+++ src/main/java/AsynchronousFramework/RxJava/TestObservablePushEvents.java
@@ -0,0 +1,33 @@
+package AsynchronousFramework.RxJava;
+
+import io.reactivex.rxjava3.core.Observable;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestObservablePushEvents {
+    public static void main(String[] args) {
+        Observable<Long> secondIntervals = Observable.interval(1, TimeUnit.SECONDS);
+        secondIntervals.subscribe(s-> System.out.println(s));
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    /* ! On a side note, we will get more into concurrency later, but we had to create a sleep() method
+* because this Observable fires emissions on a computation thread when subscribed to. The main thread
+* used to launch our application is not going to wait on this Observable since it fires on a computation
+* thread, not the main thread. Therefore, we use sleep() to pause the main thread for 5000
+* milliseconds and then allow it to reach the end of the main() method (which will cause the application
+? to terminate). This gives Observable.interval() a chance to fire for a five second window before the
+? application quits.*/
+
+    /* ! Of course, beyond map(), there are hundreds of operators in RxJava, and we
+    ! will learn about the key ones in this book. Learning which operators to use for a situation and how
+    ! to combine them is the key to mastering RxJava.
+*/
+}
+
+
+
+
--- src/main/java/AsynchronousFramework/RxJava/TestRxJava.java
+++ src/main/java/AsynchronousFramework/RxJava/TestRxJava.java
@@ -5,7 +5,33 @@ import io.reactivex.rxjava3.core.Observable;
 public class TestRxJava {
     public static void main(String[] args) {
         Observable.just("Hello world").subscribe(System.out::println);
+        /*An Observable can push
+        data or events from virtually any source  whether it is a database query or live Twitter feeds */
 
+        /*To
+         make this Observable actually push these five strings (which are called emissions we need an Observer to
+         subscribe to it and receive the items
+
+        We can quickly create and connect an Observer by passing a lambda
+        expression that specifies what to do with each string it receives:
+         */
+        Observable<String> myStrings = Observable.just("curry", "james", "durant");
+        myStrings.subscribe(System.out::println);
+        myStrings.map(s -> s.length()).subscribe(s ->
+                System.out.println(s));
+        /*
+         *If you have used Java 8 Streams or Kotlin sequences, you might be wondering how Observable is any
+         * different.
+         * */
+        /*
+         * The key difference is that Observable pushes the items while Streams and sequences pull the items
+         */
+
+        /*
+         *
+         * This may seem subtle, but the impact of a push-based iteration is far more powerful than a pull-based one.
+         * As we saw earlier, you can push not only data, but also events.
+         */
 
     }
 }
