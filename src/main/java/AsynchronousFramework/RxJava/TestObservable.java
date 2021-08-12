0502a9e23ac52f8d5d295cd569db7d65cf8dc2d2
RazorSnow <changjiangmingxj@163.com>
2021-08-12 14:04:18 +0800

    have fun

--- src/main/java/AsynchronousFramework/RxJava/TestObservable.java
+++ src/main/java/AsynchronousFramework/RxJava/TestObservable.java
@@ -13,13 +13,12 @@ public class TestObservable {
             emitter.onComplete();
         });
         source.subscribe(s -> System.out.println("RECEIVED: " + s));
-/*Note that the Observable contract dictates that
-emissions must be passed sequentially and one at a time. Emissions cannot be passed by
-an Observable concurrently or in parallel. This may seem like a limitation, but it does in
-fact simplify programs and make Rx easier to reason with. We will learn some powerful
-tricks to effectively leverage concurrency and parallelization in Chapter 6, Concurrency
-and Parallelization , without breaking the Observable contract.
+/* ? Note that the Observable contract dictates that
+ !  emissions must be passed sequentially and one at a time. Emissions cannot be passed by
+ !  an Observable concurrently or in parallel. This may seem like a limitation, but it does in
+ !  fact simplify programs and make Rx easier to reason with. We will learn some powerful
+ !  tricks to effectively leverage concurrency and parallelization in Chapter 6, Concurrency
+ !  and Parallelization , without breaking the Observable contract.
 */
-
     }
 }
--- src/main/java/Stream/GetPair.java
+++ src/main/java/Stream/GetPair.java
@@ -6,6 +6,13 @@ import java.util.List;
 import static java.util.stream.Collectors.toList;
 
 public class GetPair {
+    /*
+    ! so with the flatMap we can combine the 1 to many pair .
+    ? in this example . we have 1 to 1-3 and 1-4
+      so with the flatMap to i and to j
+    !
+    *
+    * */
     public static void main(String[] args) {
         List<Integer> numbers1 = Arrays.asList(1, 2, 3);
         List<Integer> numbers2 = Arrays.asList(3, 4);
@@ -15,7 +22,9 @@ public class GetPair {
                                 .map(j -> new int[]{i, j})
                         )
                         .collect(toList());
-        numbers1.stream()
+
+        pairs.forEach(pair -> System.out.println(Arrays.toString(pair)));
+       /* numbers1.stream()
                 .flatMap(i -> numbers2.stream()
                         .filter(j -> (i + j) % 3 == 0)
                         .map(j -> new int[]{i, j})
@@ -24,6 +33,6 @@ public class GetPair {
         numbers1.stream()
                 .flatMap(i -> numbers2.stream()
                         .map(j -> new int[]{i, j})
-                ).forEach(a -> System.out.println("[" + a[0] + "," + a[1] + "]"));
+                ).forEach(a -> System.out.println("[" + a[0] + "," + a[1] + "]"));*/
     }
 }
