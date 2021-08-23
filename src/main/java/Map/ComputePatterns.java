23091710800444c651e9e43f0d28017697568c26
Razor <changjiangmingxj@163.com>
2021-08-23 10:35:39 +0800

    java 8 特性简单学习

---
+++ src/main/java/Map/ComputePatterns.java
@@ -0,0 +1,16 @@
+package Map;
+
+public class ComputePatterns {
+    /*
+     * Sometimes, you want to perform an operation conditionally and store its result,
+     * depending on whether a key is present or absent in a Map
+     * what kind of the operation will be ? in this type ?
+     *
+     *
+     * */
+    public static void main(String[] args) {
+
+
+
+    }
+}
