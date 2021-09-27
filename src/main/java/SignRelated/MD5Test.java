32da7d0d4eaaf180a9aa67f391df16240cf6e124
常江铭 <changjiangming@haoma.com>
2021-09-27 18:32:35 +0800

    9-27-2021

---
+++ src/main/java/SignRelated/MD5Test.java
@@ -0,0 +1,23 @@
+package SignRelated;
+
+import org.apache.commons.lang3.StringUtils;
+
+import javax.xml.bind.DatatypeConverter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5Test {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        String hash = "35454B055CC325EA1AF2126E27707052";
+        String password = "ILoveJava";
+
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(password.getBytes());
+        byte[] digest = md.digest();
+        String myHash = DatatypeConverter
+                .printHexBinary(digest).toUpperCase();
+        System.out.println(myHash);
+        boolean isSame = StringUtils.equals(hash,myHash);
+        System.out.println(isSame);
+    }
+}
